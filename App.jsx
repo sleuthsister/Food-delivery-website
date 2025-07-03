f661a438be689c209bc3ea9d575ec375a3b59537
ritikaaaa <23bce289@nirmauni.ac.in>
2025-07-03 10:10:50 +0400

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,28 +1,32 @@
-import React, {useState} from 'react'
+import React from 'react'
 import Navbar from './components/Navbar/Navbar'
-import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home/Home'
-import Cart from './pages/Cart/Cart'
-import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
-import Footer from 'src/components/Footer/Footer'
-import LoginPopup from 'src/components/LoginPopup/LoginPopup'
+import Sidebar from './components/Sidebar/Sidebar'
+import {Routes,Route} from 'react-router-dom'
+import Add from './pages/Add/Add'
+import List from './pages/List/List'
+import Orders from './pages/Orders/Orders'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 const App = () => {
-  const [showLogin,setShowLogin]=useState(false)
+
+  const url='http://localhost:4000'
   return (
-    <>
-    {showLogin?<LoginPopup setShowLogin={setShowLogin}/>:<></>}
-    <div className='app'>
-      <Navbar setShowLogin={setShowLogin}/>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/cart' element={<Cart/>}/>
-        <Route path='/order' element={<PlaceOrder/>}/>
-      </Routes>
+    <div>
+      <ToastContainer/>
+      <Navbar/>
+      <hr />
+      <div className="app-content">
+        <Sidebar/>
+        <Routes>
+          <Route path='/add' element={<Add url={url}/>}/>
+          <Route path='/list' element={<List url={url}/>}/>
+          <Route path='/orders' element={<Orders url={url}/>}/>
+        </Routes>
+      </div>
+      
     </div>
-    <Footer/>
-    </>
-    
   )
 }
 
